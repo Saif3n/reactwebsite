cd093b8361e56d308720cfc259b5b33087d814aa
Nicholas <nicholaswu498@gmail.com>
2022-09-06 21:35:47 +1200

    changing form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,6 @@ import axios from "axios";
 
 
 function Contact() {
-  const [validated, setValidated] = useState(false);
 
   const name = useRef("");
   const message = useRef("");
@@ -33,12 +32,12 @@ function Contact() {
       <h1 class="contactheader">Contact me:</h1>
       <Form>
         <Form.Group class="box" controlId="validationName">
-          <Form.Control required ref={name} id="mb-3" type="text" placeholder=" First Name*"  />
+          <Form.Control required id="mb-3" type="text" placeholder=" First Name*" ref={name}  />
         </Form.Group>
         <br></br>
         <Form.Group controlId="validationEmail">
 
-          <Form.Control required type={email} id="mb-3" placeholder=" Email Address*" defaultValue="nicholaswu498@gmail.com"  />
+          <Form.Control required id="mb-3" placeholder=" Email Address*" defaultValue="nicholaswu498@gmail.com"  ref={email}/>
           
         </Form.Group>
         <br></br>
